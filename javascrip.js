b4de2f753ab6ae09a66b386d288d17b767025a6c
SiwaSathya <siwasathya@gmail.com>
2023-01-22 22:26:51 +0800

    sip jadi

---
+++ javascrip.js
@@ -0,0 +1,52 @@
+$(function () {
+    $("#checkout").datepicker();
+    $("#checkin").datepicker();
+  });
+
+   function booking(namaObjek, harga) {
+    $("#modalForm").modal("show");
+    $("#judulModal").text(namaObjek);
+    $("#harga").val(harga);
+  }
+
+  function sweet() {
+    Swal.fire("Booked", "Wait for next information", "success");
+  }
+
+  $("#qty").on("input", function (e) {
+    var qty = $("#qty").val();
+    var harga = $("#harga").val().replace(/\./g, "");
+    var total = parseInt(harga) * parseInt(qty);
+    $("#total").val(total.toLocaleString());
+    // alert (harga);
+  });
+
+   $("#cari").click(function () {
+    var jenis = $("#jenis").val();
+    if (jenis == "Hotel") {
+      $("#divHotel").fadeIn("1000");
+      $("#divKendaraan").fadeOut()    ;
+    } else if (jenis == "Kendaraan") {
+      $("#divHotel").fadeOut();
+      $("#divKendaraan").fadeIn("15000");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="">Why do I have this issue?</a>',
+      });
+    }
+  });
+
+  $(document).ready(function(){
+    window.setInterval(function () {
+        var sisawaktu = $("#waktu").html();
+        sisawaktu = eval(sisawaktu);
+        if (sisawaktu == 0) {
+            location.href = "http://achmatim.net/";
+        } else {
+            $("#waktu").html(sisawaktu - 1);
+        }
+    }, 1000);
+});
