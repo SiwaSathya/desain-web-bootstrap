aeec1b556710e1b3a10a1c3d8479978f998d8abf
SiwaSathya <siwasathya@gmail.com>
2023-04-08 00:19:37 +0800

    make sure changes, maybe include some bug

--- javascrip.js
+++ javascrip.js
@@ -50,3 +50,10 @@ $(function () {
         }
     }, 1000);
 });
+
+function delayFunction() {
+    // tambahkan delay sebelum submit form
+    setTimeout(function() {
+      document.querySelector('form').submit();
+    }, 2000); // 3000 milidetik atau 3 detik delay
+  }
